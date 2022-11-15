ecc3285ee7b82b6a3f57443491a2564ab0650159
Harold Jean <haroldjean38@yahoo.com>
2022-11-15 12:35:53 -0500

    added render and fetch file

--- fetch-utils.js
+++ fetch-utils.js
@@ -5,16 +5,16 @@ const SUPABASE_KEY =
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function getDogs() {
-    // from the dogs table, select all items'
-    const response = await client.from('dogs').select('*');
+export async function getPokemons() {
+    // from the pokemons table, select all items'
+    const response = await client.from('pokemons').select('*');
     // and return the response
     return response;
 }
 
-export async function getDog(id) {
-    // from the dogs table, select a single dog who has the matching id
-    const response = await client.from('dogs').select().match({ id: id }).single();
+export async function getPokemon(id) {
+    // from the pokemons table, select a single dog who has the matching id
+    const response = await client.from('pokemons').select().match({ id: id }).single();
     // and return the response
     return response;
 }
