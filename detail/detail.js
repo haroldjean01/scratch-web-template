88d5d99a2db1ea32681571b4102dcc3f85bb20d7
Harold Jean <haroldjean38@yahoo.com>
2022-11-14 20:57:09 -0500

    completed html

---
+++ detail/detail.js
@@ -0,0 +1,17 @@
+import { getDog } from '../fetch-utils.js';
+import { renderDogDetail } from '../render-utils.js';
+
+const dogDetailContainer = document.getElementById('dog-detail-container');
+
+// on load
+window.addEventListener('load', async () => {
+    // get the id from URL
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+
+    // use the id to fetch the dog
+    const dog = await getDog(id);
+    // render and append this dog's details to the container
+    const dogDetail = renderDogDetail(dog.data);
+    dogDetailContainer.append(dogDetail);
+});
